1cc5b96c72cce54982a57d4f8f24b0c7df208517
Paulo Guilherme Santiago <37718188+PauloMachine@users.noreply.github.com>
2022-01-29 23:40:22 -0300

    feat: integration gmail

---
+++ integration_gmail.py
@@ -0,0 +1,33 @@
+import imaplib
+import email
+import time
+
+from robot.api.deco import keyword
+
+HOST = "imap.gmail.com"
+USERNAME = "email"
+PASSWORD = "senha"
+
+@keyword(name="Checking Email Sending")
+def checking_email_sending():
+  return get_last_email()
+
+def get_last_email():
+  mail = imaplib.IMAP4_SSL(HOST)
+  mail.login(USERNAME, PASSWORD)
+
+  _, num_messages = mail.select("inbox")
+  _, last_message = mail.fetch(num_messages[0], '(RFC822)')
+  _, bytes_message = last_message[0]
+
+  message = {}
+
+  email_message = email.message_from_bytes(bytes_message)
+  message['subject'] = email_message['subject']
+
+  for part in email_message.walk():
+    if part.get_content_type() == "text/plain":
+      body = part.get_payload(decode=True)
+      message['body'] = body.decode().replace("\r\n", "").split()
+
+  return message
